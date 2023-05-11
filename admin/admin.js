219dedb01003858d787efeb45a815c232bf95390
Sofiane <sofiane.aboulkabila@outlook.com>
2023-05-11 14:18:36 +0200

    ..

--- admin/admin.js
+++ admin/admin.js
@@ -1,61 +1,180 @@
-const formAdd = document.getElementById('add-form');
-formAdd.addEventListener('submit', (event) => {
+// LIRE ET AFFICHER DANS TABLEAU
+
+fetch('/projets')
+    .then(response => response.json())
+    .then(data => {
+        const tableBody = document.querySelector('#projects-table');
+
+        data.forEach(project => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+          <td>${project.name}</td>
+          <td>${project.link}</td>
+          <td>${project.img}</td>
+          <td>${project.skills}</td>
+          <td>
+            <button onclick="editProject('${project.name}')">Modifier</button>
+            <button onclick="deleteProject('${project.name}')">Supprimer</button>
+          </td>
+        `;
+            tableBody.appendChild(row);
+        });
+    })
+    .catch(error => console.error(error));
+
+//   AJOUT A DATA.JSON
+
+const form = document.getElementById('add-form');
+form.addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    const name = document.getElementById('add-name').value;
-    const link = document.getElementById('add-link').value;
-    const img = document.getElementById('add-img').value;
-    const skills = document.getElementById('add-skills').value.split(',');
+    const formData = new FormData(form);
+    const newProjet = {};
 
-    const newProject = {
-        id: Date.now(),
-        name,
-        link,
-        img,
-        skills
-    };
+    for (let [key, value] of formData.entries()) {
+        newProjet[key] = value;
+    }
 
-    fetch("/cartes", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newProject),
-    })
-        .then((response) => {
-            if (response.ok) {
-                console.log("Carte ajoutée avec succès");
-                formAdd.reset();
-                location.reload();
-            } else {
-                console.error("Erreur lors de l'ajout de la carte");
-            }
-        })
-        .catch((error) => console.error(error));
+    try {
+        const response = await fetch('/projets/:name', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newProjet)
+        });
+
+        if (!response.ok) {
+            throw new Error('Erreur lors de l\'ajout du projet');
+        }
+
+        alert('Projet ajouté avec succès');
+        window.location.reload();
+    } catch (error) {
+        console.error(error);
+        alert('Une erreur est survenue lors de l\'ajout du projet');
+    }
 });
 
-function updateTable() {
-    fetch('/data.json')
-        .then(response => response.json())
-        .then(data => {
-            const tableBody = document.getElementById('projects-table');
-            tableBody.innerHTML = ''; // clear table body
-            data.forEach(project => {
-                const newRow = tableBody.insertRow(-1);
-                newRow.innerHTML = `
-                    <td>${project.name}</td>
-                    <td><a href="${project.link}" target="_blank">${project.link}</a></td>
-                    <td><img src="${project.img}" alt="${project.name}"></td>
-                    <td>${project.skills.join(', ')}</td>
-                    <td>
-                        <button class="edit-button">Modifier</button>
-                        <button class="delete-button">Supprimer</button>
-                    </td>
-                `;
+
+
+// SUPPRIMER DE DATA.JSON
+
+function deleteProject(name) {
+    if (confirm(`Voulez-vous supprimer le projet ${name} ?`)) {
+        fetch(`/projets/${name}`, { method: 'DELETE' })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erreur lors de la suppression du projet');
+                }
+                alert('Projet supprimé avec succès');
+                window.location.reload(); // recharger la page après la suppression
+            })
+            .catch(error => {
+                console.error(error);
+                alert('Une erreur est survenue lors de la suppression du projet');
             });
-        })
-        .catch(error => console.error(error));
+    }
 }
 
-// Mettre à jour le tableau lors du chargement de la page
-updateTable();
+
+const editForm = document.createElement('form');
+const nameLabel = document.createElement('label');
+const nameInput = document.createElement('input');
+const linkLabel = document.createElement('label');
+const linkInput = document.createElement('input');
+const imgLabel = document.createElement('label');
+const imgInput = document.createElement('input');
+const skillsLabel = document.createElement('label');
+const skillsInput = document.createElement('input');
+const submitButton = document.createElement('button');
+
+function editProject(name) {
+    // Envoyer une requête GET pour récupérer les informations du projet
+    fetch(`/projets/${name}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erreur lors de la récupération des informations du projet : ' + response.statusText);
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Remplir les champs avec les informations du projet
+        nameInput.value = data.name;
+        linkInput.value = data.link;
+        imgInput.value = data.img;
+        skillsInput.value = data.skills;
+  
+        // Personnaliser les éléments de l'interface utilisateur
+        editForm.setAttribute('id', 'edit-form');
+        nameLabel.textContent = 'Nom du projet : ';
+        nameInput.setAttribute('type', 'text');
+        nameInput.setAttribute('name', 'name');
+        linkLabel.textContent = 'Lien : '; // Ajouter une balise label pour le champ link
+        linkInput.setAttribute('type', 'text');
+        linkInput.setAttribute('name', 'link');
+        imgLabel.textContent = 'Image : '; // Ajouter une balise label pour le champ img
+        imgInput.setAttribute('type', 'text');
+        imgInput.setAttribute('name', 'img');
+        skillsLabel.textContent = 'Compétences : '; // Ajouter une balise label pour le champ skills
+        skillsInput.setAttribute('type', 'text');
+        skillsInput.setAttribute('name', 'skills');
+        submitButton.textContent = 'Mettre à jour le projet';
+  
+        // Ajouter les éléments au formulaire
+        nameLabel.appendChild(nameInput);
+        linkLabel.appendChild(linkInput);
+        imgLabel.appendChild(imgInput);
+        skillsLabel.appendChild(skillsInput);
+        editForm.appendChild(nameLabel);
+        editForm.appendChild(linkLabel);
+        editForm.appendChild(imgLabel);
+        editForm.appendChild(skillsLabel);
+        editForm.appendChild(submitButton);
+  
+        // Ajouter un gestionnaire d'événement pour soumettre le formulaire
+        editForm.addEventListener('submit', event => {
+          event.preventDefault(); // Empêcher le comportement par défaut du navigateur
+          const updatedProject = {
+            name: nameInput.value,
+            link: linkInput.value,
+            img: imgInput.value,
+            skills: skillsInput.value,
+          };
+          // Envoyer une requête PUT pour mettre à jour le projet
+          fetch(`/projets/modify/${name}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedProject),
+          })
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Erreur lors de la mise à jour du projet : ' + response.statusText);
+              }
+              return response.json();
+            })
+            .then(data => {
+              console.log(data);
+              alert('Le projet a été mis à jour avec succès !');
+            })
+            .catch(error => {
+              console.error(error);
+              alert('Une erreur est survenue lors de la mise à jour du projet.');
+            });
+        });
+  
+        // Ajouter le formulaire à la page HTML
+        const editSection = document.getElementById('edit-section');
+        if (editSection) {
+          editSection.innerHTML = '';
+          editSection.appendChild(editForm);
+        }
+      })
+    .catch(error => {
+      console.error(error);
+      // Afficher un message d'erreur à l'utilisateur
+      alert(error.message);
+    });
+}
--- index.js
+++ index.js
@@ -1,66 +1,162 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
-const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 4008;
 
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve static files from the "admin" folder
+app.use('/admin', express.static('admin'));
+
+// Route pour récupérer toutes les données
 app.get('/projets', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'admin', 'admin.php'));
-});
-
-// LIRE
-app.get('/data.json', (req, res) => {
-  fs.readFile('./data.json', 'utf8', (err, data) => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      res.status(500).send('Internal Server Error');
-      return;
+      res.status(500).send('Erreur serveur');
+    } else {
+      const projets = JSON.parse(data);
+      res.json(projets);
     }
-
-    res.json(JSON.parse(data));
   });
 });
 
-// AJOUTER
-app.post('/projets/add', (req, res) => {
-  // Read the data from the file
-  fs.readFile('./data.json', 'utf8', (err, data) => {
+//  Route pour récupérer toutes 
+
+app.get('/projets/:name', (req, res) => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      res.status(500).send('Internal Server Error');
+      res.status(500).send('Erreur lors de la récupération des projets');
       return;
     }
 
-    const projetData = JSON.parse(data);
+    const projets = JSON.parse(data);
+    const projetName = req.params.name;
+    const projet = projets.find(projet => projet.name.toLowerCase() === projetName.toLowerCase());
 
-    // Add the new project to the data
-    const newProject = {
-      id: Date.now(),
-      name: req.body.name,
-      link: req.body.link,
-      img: req.body.img,
-      skills: req.body.skills.split(',')
-    };
-    projetData.push(newProject);
+    if (!projet) {
+      res.status(404).send('Projet non trouvé');
+    } else {
+      res.send(projet);
+    }
+  });
+});
 
-    // Write the updated data back to the file
-    fs.writeFile('./data.json', JSON.stringify(projetData), 'utf8', (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-        return;
+
+
+
+
+
+//  Route pour ajouter un nouveau projet 
+
+app.post('/projets/:name', (req, res) => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Erreur lors de la récupération des projets');
+    } else {
+      const projets = JSON.parse(data);
+      const newProjet = req.body;
+
+      // Vérifier si un projet avec le même nom existe déjà
+      const existingProjet = projets.find(projet => projet.name === newProjet.name);
+      if (existingProjet) {
+        res.status(400).send('Un projet avec le même nom existe déjà');
+      } else {
+        projets.push(newProjet);
+        fs.writeFile('data.json', JSON.stringify(projets), err => {
+          if (err) {
+            console.error(err);
+            res.status(500).send('Erreur lors de l\'ajout du projet');
+          } else {
+            res.send('Projet ajouté avec succès');
+          }
+        });
       }
-
-      res.status(200).send('OK');
-    });
+    }
   });
 });
+
+
+// Route pour modifier un projet existant 
+
+// Route pour modifier un projet existant 
+app.put('/projets/modify/:name', (req, res) => {
+  const projetName = req.params.name;
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Erreur lors de la récupération des projets');
+    } else {
+      const projets = JSON.parse(data);
+      const projetToUpdate = projets.find(projet => projet.name === projetName);
+      if (!projetToUpdate) {
+        res.status(404).send('Projet non trouvé');
+      } else {
+        const updatedProjet = req.body;
+        Object.assign(projetToUpdate, updatedProjet);
+        fs.writeFile('data.json', JSON.stringify(projets), err => {
+          if (err) {
+            console.error(err);
+            res.status(500).send('Erreur lors de la mise à jour du projet');
+          } else {
+            console.log('Projet mis à jour avec succès:', JSON.stringify(updatedProjet));
+            res.send('Projet mis à jour avec succès');
+          }
+        });
+      }
+    }
+  });
+});
+
+
+// Route pour supprimer un projet existant :
+app.delete('/projets/:name', (req, res) => {
+  const projectName = req.params.name;
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Erreur lors de la récupération des projets');
+    } else {
+      const projets = JSON.parse(data);
+      const projetIndex = projets.findIndex(projet => projet.name === projectName);
+      if (projetIndex === -1) {
+        res.status(404).send('Projet non trouvé');
+      } else {
+        projets.splice(projetIndex, 1);
+        fs.writeFile('data.json', JSON.stringify(projets), err => {
+          if (err) {
+            console.error(err);
+            res.status(500).send('Erreur lors de la suppression du projet');
+          } else {
+            res.send('Projet supprimé avec succès');
+          }
+        });
+      }
+    }
+  });
+});
+
+
+// Lire le contenu de data.json
+fs.readFile('data.json', 'utf8', (err, data) => {
+  if (err) throw err;
+
+  // Convertir le contenu JSON en un objet JavaScript
+  const projects = JSON.parse(data);
+
+  // Utiliser les données récupérées
+  console.log(projects);
+});
+
+
+// Endpoint par défaut pour toutes les autres requêtes
+app.use((req, res) => {
+  res.status(404).send('Page introuvable');
+});
+
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
